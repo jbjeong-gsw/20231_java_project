3ad7bff649bf6eaa5920c5b272b49374dddd9a3e
정재봉 <batang21@gmail.com>
2023-03-07 11:11:34 +0900

    silplest 프로젝트 생성

---
+++ simplest/src/test/java/kr/hs/gsw/web/simplest/SimplestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.gsw.web.simplest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
