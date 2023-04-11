20f9c8e13f57af16e09314c2266c5fca1060271b
Woogong <batang21@gmail.com>
2023-04-11 15:16:17 +0900

    4월 11일 2반

--- database2/src/main/java/kr/hs/gbsw/database/controller/UserController.java
+++ database2/src/main/java/kr/hs/gbsw/database/controller/UserController.java
@@ -2,19 +2,17 @@ package kr.hs.gbsw.database.controller;
 
 import kr.hs.gbsw.database.entity.User;
 import kr.hs.gbsw.database.repository.UserRepository;
+import kr.hs.gbsw.database.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/user/add")
     public String addUser(Model model) {
@@ -23,8 +21,18 @@ public class UserController {
 
     @PostMapping("/user/add")
     public String addUser(@ModelAttribute User user) {
-        userRepository.save(user);
+        userService.save(user);
 
         return "redirect:/user/add";
+    }
+
+    @RequestMapping("/user/detail")
+    public String showUserDetail(Model model,
+            @RequestParam(value = "id") int id) {
+
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+
+        return "user/detail";
     }
 }
