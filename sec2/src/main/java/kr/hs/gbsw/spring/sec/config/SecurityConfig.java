3725a3fa9e0819ebc3fb6c7b14270ea645345641
Woogong <batang21@gmail.com>
2023-05-09 15:19:10 +0900

    5월 9일 2반

---
+++ sec2/src/main/java/kr/hs/gbsw/spring/sec/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package kr.hs.gbsw.spring.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/page1").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+}
